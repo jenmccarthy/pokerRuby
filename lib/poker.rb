28486efe088b2e3ed27456d60711d9b286b0105b
jenmccarthy <jennifernward@gmail.com>
2014-07-24 13:23:21 -0700

    all poker hand methods passing

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,41 +1,160 @@
-def pokerHand(hand)
-  result = ""
+def is_straight(hand)
+
+  result = "straight"
+
   hand = hand.sort
 
-  straight = nil
+  hand.each do |card|
+    card.chop!
+  end
 
   hand.each_with_index do |card, index|
-    if index < hand.length - 1
-      if card[0].to_i == (hand[index + 1][0].to_i - 1)
-        straight = true
-      else
-        straight = false
+     if index < hand.length - 1
+      if (card.to_i) + 1 == card.to_i
+        result
       end
     end
   end
-  if straight
-    'straight'
+   result
+end
+
+def flush(hand)
+  result = "flush"
+
+  hand.each_with_index do |card, index|
+      if index < hand.length - 1
+        if card[-1] == hand[index + 1][-1]
+           result
+        end
+      end
+  end
+  result
+end
+
+def straight_flush(hand)
+  hand = hand.sort
+  result = "straight flush"
+
+  if is_straight(hand) && flush(hand)
+    result
+  end
+  result
+end
+
+def four_kind(hand)
+  hand = hand.sort
+  result = "four of a kind"
+
+  match_array = []
+
+  hand.each do |card|
+    card.chop!
+  end
+
+  hand.each_with_index do |card, index|
+    if card == (hand[index + 1])
+      match_array.push(card)
+      if match_array.length == 4
+        result
+      end
+    end
+  end
+  result
+end
+
+def three_kind(hand)
+  hand = hand.sort
+  result = "three of a kind"
+
+  match_array = []
+
+  hand.each do |card|
+    card.chop!
+  end
+
+  hand.each_with_index do |card, index|
+    if card == (hand[index + 1])
+      match_array.push(card)
+      if match_array.length == 3
+        result
+      end
+    end
+  end
+  result
+end
+
+def one_pair(hand)
+  result = "one pair"
+
+  match_array = []
+
+  hand = hand.sort
+
+  hand.each do |card|
+    card.chop!
+  end
+
+  hand.each_with_index do |card, index|
+    if card == (hand[index + 1])
+      match_array.push(card)
+      if match_array.length == 2
+        result
+      end
+    end
+  end
+  result
+end
+
+def full_house(hand)
+  result = "full house"
+  hand = hand.sort
+
+  if three_kind(hand) && one_pair(hand)
+    result
+  end
+  result
+end
+
+def two_pair(hand)
+  result ="two pair"
+
+  first_match_array = []
+  second_match_array = []
+
+  hand.each_with_index do |card, index|
+    if card == hand[index + 1]
+      first_match_array.slice![index]
+    end
+  end
+  hand.each_with_index do |card, index|
+    if card == hand[index + 1]
+      second_match_array.slice![index]
+    end
+  end
+
+  if first_match_array.length == second_match_array.length
+    result
+  end
+  result
+end
+
+def poker_hand(hand)
+
+  final_result = "You do not have a good hand :("
+
+  if straight_flush(hand)
+  elsif is_straight(hand)
+  elsif flush(hand)
+  elsif four_kind(hand)
+  elsif three_kind(hand)
+  elsif one_pair(hand)
+  elsif full_house(hand)
+  elsif two_pair(hand)
+  else
+    final_result
   end
 end
 
+# suit_count = {"C" => 0, "D" => 0, "H" => 0, "S" => 0}
 
-pokerHand(["1H", "3C", "4S", "2H", "5D"])
-
-
-#user inputs a string of cards which we want to become an array with 5 elements
-#["card1", "card2", "card3", "card4", "card5"]
-
-
-#card1[0] is the numerical part of the string and for the majority of poker hands, only the value is
-#essential to know the optimal poker hand
-
-#so we will isolate the 0th element of each card, push that into a new array and sort it.
-#at which point we can see if the cards increment by one (straight) or have things in common (two/three of a kind), etc
-
-#then to isolate the suit, loop through the last element of the array (in this case position 1) and push those into a new array
-#we will sort this information to see patterns amongst the suits
-
-#now we have three arrays--one with card values and one with card suits and one with the original user input
-
-
-
+  # value_count = {"1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0, "11" => 0, "12" => 0 "13" => 0, "14" => 0}
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -1,9 +1,60 @@
 require('rspec')
 require('poker')
 
-describe("pokerHand") do
-  it("will return the correct poker hand for corresponding user input") do
-    pokerHand(["1H", "3C", "4S", "2H", "5D"]).should(eq("straight"))
+describe("is_straight") do
+  it("will return the correct poker hand for card values that are sequential") do
+    is_straight(["10H", "11C", "12S", "13H", "14D"]).should(eq("straight"))
   end
 end
+
+describe("straight_flush") do
+    it("will return the correct poker hand for values that are sequential and with matching suits") do
+      straight_flush(["4C","7C","3C","6C","5C"]).should(eq("straight flush"))
+    end
+end
+
+describe("flush") do
+    it("will return the correct poker hand for cards with all the same suits") do
+    flush(["14C","11C","10C","6C","3C"]).should(eq("flush"))
+    end
+end
+
+describe("four_kind") do
+   it("will return the correct poker hand for cards with four of a kind") do
+    four_kind(["6C","6D","6H","6S","14C"]).should(eq("four of a kind"))
+  end
+end
+
+describe("three_kind") do
+  it("will return the correct poker hand for cards with three of a kind") do
+    three_kind(["3C","3D","3H","12S","2S"]).should(eq("three of a kind"))
+  end
+end
+
+describe("one_pair") do
+  it("will return the correct poker hand for cards with one pair") do
+    one_pair(["2D","2H","12C","10H","6C"]).should(eq("one pair"))
+  end
+end
+
+describe("full_house") do
+  it("will return the correct poker hand for cards with 3 values and 2 values the same") do
+    full_house(["5S","5H","5D","8S","8H"]).should(eq("full house"))
+  end
+end
+
+describe("two_pair") do
+  it("will return the correct poker hand for cards with two pair") do
+    two_pair(["13C","13D","9H","9D","11H"]).should(eq("two pair"))
+  end
+end
+
+describe("poker_hand") do
+  it("will return the hand you are holding") do
+    poker_hand(["3C","3D","3H","12S","2S"]).should(eq("three of a kind"))
+  end
+end
+
+
+
 
